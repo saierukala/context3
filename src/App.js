47fd5e7899036d8fc8fc7391dcbb22311380aab0
saierukala <sai.erukala1508@gamil.com>
2023-02-03 16:33:02 +0530

    context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import ProductItemDetails from './components/ProductItemDetails'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    cartList: [],
+  }
+
+  addCartItem = product => {
+    console.log(product)
+    this.setState(prevState => ({cartList: [...prevState.cartList, product]}))
+  }
+
+  render() {
+    const {cartList} = this.state
+
+    return (
+      <BrowserRouter>
+        <CartContext.Provider
+          value={{
+            cartList,
+            addCartItem: this.addCartItem,
+            deleteCartItem: this.deleteCartItem,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Switch>
+            <Route exact path="/login" component={LoginForm} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/products" component={Products} />
+            <ProtectedRoute
+              exact
+              path="/products/:id"
+              component={ProductItemDetails}
+            />
+            <ProtectedRoute exact path="/cart" component={Cart} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="not-found" />
+          </Switch>
+        </CartContext.Provider>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default App
